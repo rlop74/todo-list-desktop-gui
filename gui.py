f0d9f0dba098fa9a00afef1f25491e133b676f10
rlop74 <119091421+rlop74@users.noreply.github.com>
2025-07-24 14:52:30 -0400

    change theme, add clock, handled edit and complete IndexError

--- gui.py
+++ gui.py
@@ -1,6 +1,10 @@
 import functions
 import FreeSimpleGUI as sg
+import time
 
+sg.theme("DarkBlack")
+
+clock = sg.Text("", key="clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
@@ -12,6 +16,7 @@ exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-Do App",
                    layout=[
+                       [clock],
                        [label],
                        [input_box, add_button],
                        [list_box, edit_button, complete_button],
@@ -19,9 +24,8 @@ window = sg.Window("My To-Do App",
                    ],
                    font=("Helvetica", 20))
 while True:
-    event, values = window.read()
-    print(event)
-    print(values)
+    event, values = window.read(timeout=200)
+    window["clock"].update(value=time.strftime("%b %d %Y  %H:%M:%S"))
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -33,29 +37,35 @@ while True:
             window["todos"].update(values=todos)
 
         case "Edit":
-            todo_to_edit = values["todos"][0]
-            new_todo = values["todo"] + "\n"
+            try:
+                todo_to_edit = values["todos"][0]
+                new_todo = values["todo"] + "\n"
 
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
 
-            # update the list in real-time
-            window["todos"].update(values=todos)
+                # update the list in real-time
+                window["todos"].update(values=todos)
+            except IndexError:
+                sg.popup("Please select an item first.", font=("Helvetica", 20))
 
         case "Complete":
-            # parse todo to complete and delete from list
-            todo_to_complete = values["todos"][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
+            try:
+                # parse todo to complete and delete from list
+                todo_to_complete = values["todos"][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
 
-            # update list box in real time
-            window["todos"].update(values=todos)
+                # update list box in real time
+                window["todos"].update(values=todos)
 
-            # clear the input box
-            window["todo"].update("")
+                # clear the input box
+                window["todo"].update("")
+            except IndexError:
+                sg.popup("Please select an item first.", font=("Helvetica", 20))
 
         case "Exit":
             break
